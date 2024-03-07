87400e82c4eda83d4c70801d7d16dd9d411dd3e0
Fabian Schmid <fabian@sr.solutions>
2024-03-07 21:19:19 +0100

    [FIX] Support CLI installation

--- plugin.php
+++ plugin.php
@@ -8,4 +8,4 @@ $ilias_min_version = '7.0';
 $ilias_max_version = '7.999';
 $responsible = 'sr.solutions';
 $responsible_mail = 'support@sr.solutions';
-$supports_cli_setup = false;
+$supports_cli_setup = true;
