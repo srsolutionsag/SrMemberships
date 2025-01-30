e167c49d6bb594433ceef4322e49f9ab9675e03c
Lukas Zehnder <lukas@sr.solutions>
2025-01-30 13:21:02 +0100

    [FIX] PLSRMEM-13: Fixed issue with account creation

--- src/AccountCreator.php
+++ src/AccountCreator.php
@@ -61,7 +61,7 @@ class AccountCreator
             $user->saveAsNew();
 
             foreach ($this->global_roles as $global_role) {
-                $DIC->rbac()->admin()->assignUser($user->getId(), $global_role);
+                $DIC->rbac()->admin()->assignUser((int) $global_role, $user->getId());
             }
         }
 
